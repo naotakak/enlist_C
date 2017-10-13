480f4650f45f02486e861df050c479ba76fd6bd2
Naotaka Kinoshita <naotakakinoshita@gmail.com>
2017-10-13 15:35:01 -0400

    started

---
+++ enlist.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_list(struct node *);
+struct node * insert_front(struct node *, int);
+struct node * free_list(struct node *);
+
+int main() {
+
+  return 0;
+}
+
+void print_list(struct node *) {
+
+}
+
+struct node * insert_front(struct node *, int) {
+
+}
+
+struct node * free_list(struct node *) {
+
+}
